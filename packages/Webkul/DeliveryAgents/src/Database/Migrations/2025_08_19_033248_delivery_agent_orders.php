b6c6ef815e4912d552b125a46061843ad3ca6637
Hosam Albushari <hosamalbushari@gmail.com>
2025-09-12 19:26:41 +0300

    update in config

--- packages/Webkul/DeliveryAgents/src/Database/Migrations/2025_08_19_033248_delivery_agent_orders.php
+++ packages/Webkul/DeliveryAgents/src/Database/Migrations/2025_08_19_033248_delivery_agent_orders.php
@@ -16,12 +16,13 @@ return new class extends Migration
             $table->unsignedInteger('order_id')->nullable();
             $table->unsignedInteger('delivery_agent_id')->nullable();
             $table->string('status')->nullable();
-            $table->timestamp('assigned_at')->nullable();
+            $table->longText('delivery_agent_info')->nullable();
+            $table->timestamp('assigned_at')->nullable()->collation('utf8mb4_bin');
             $table->timestamp('accepted_at')->nullable();
             $table->timestamp('rejected_at')->nullable();
             $table->timestamp('completed_at')->nullable();
             $table->timestamps();
-            $table->foreign('order_id')->references('id')->on('orders')->onDelete('set null');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->foreign('delivery_agent_id')->references('id')->on('delivery_agents')->onDelete('set null');
         });
     }
