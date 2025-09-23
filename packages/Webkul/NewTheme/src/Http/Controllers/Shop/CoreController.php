8d9467fa172ec744dcfd50de339d3ab712ed1ffa
Hosam Albushari <hosamalbushari@gmail.com>
2025-09-23 04:11:57 +0300

    update

--- packages/Webkul/NewTheme/src/Http/Controllers/Shop/CoreController.php
+++ packages/Webkul/NewTheme/src/Http/Controllers/Shop/CoreController.php
@@ -1,16 +1,17 @@
 <?php
 
+namespace Webkul\NewTheme\Http\Controllers\Shop;
+
 use Webkul\Shop\Http\Controllers\API\APIController;
 
 class CoreController extends APIController
 {
     public function getAreas()
     {
-        {
-            return response()->json([
-                'data' => myHelper()->groupedAreasByStatesCode(),
-            ]);
-        }
-    }
 
+        return response()->json([
+            'data' => myHelper()->groupedAreasByStatesCode(),
+        ]);
+
+    }
 }
--- packages/Webkul/NewTheme/src/Routes/api-routes.php
+++ packages/Webkul/NewTheme/src/Routes/api-routes.php
@@ -1,13 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Webkul\NewTheme\Http\Controllers\Shop\CoreController;
 
 Route::group(['middleware' => ['web', 'admin'], 'prefix' => 'api'], function () {
-
-
     Route::controller(CoreController::class)->prefix('core')->group(function () {
-
-
         Route::get('areas', 'getAreas')->name('shop.api.core.areas');
 
     });
